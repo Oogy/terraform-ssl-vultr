b874a3a120cd349aa07800e8d1b7586c49a91268
oogy <tylerweldon94@gmail.com>
2020-03-09 13:50:24 +0000

    First

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "acme" {
+  server_url = var.acme_url
+}
+
+resource "tls_private_key" "tpot_private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "acme_registration" "reg" {
+  account_key_pem = tls_private_key.tpot_private_key.private_key_pem
+  email_address   = var.acme_email
+}
+
+resource "acme_certificate" "cert" {
+  account_key_pem           = acme_registration.reg.account_key_pem
+  common_name               = var.cert_domain
+  subject_alternative_names = var.cert_san
+  key_type                  = 4096
+
+  dns_challenge {
+    provider = "vultr"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "acme_url" {
+  type = string
+}
+
+variable "acme_email" {
+  type = string
+}
+
+variable "cert_domain" {
+  type = string
+}
+
+variable "cert_san" {
+  type = list
+}
+
+
+
